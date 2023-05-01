129e3f14582304cc69c7c67fe1d64b705547b077
MubarakOke <okeolamubarak18@gmail.com>
2023-05-01 20:54:33 +0100

    app set up

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate piston_window;
+extern crate rand;
+
+mod draw;
+mod game;
+
+
+use piston_window::types::Color;
+use piston_window::*;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
+fn main() {
+    println!("{}", (5)*3);
+}
