98d135a35a6a96542c6d487d0f519376a35908e0
MubarakOke <okeolamubarak18@gmail.com>
2023-05-01 21:50:10 +0100

    created window screen

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,18 @@ mod game;
 
 use piston_window::types::Color;
 use piston_window::*;
+use crate::draw::to_coord;
+use crate::game::Game;
 
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 
-fn main() {
-    println!("{}", (5)*3);
+fn main(){
+    let (width, height)= (30, 30);
+    let window: PistonWindow = WindowSettings::new("Snake", [to_coord(width), to_coord(height)])
+                                .exit_on_esc(true)
+                                .build()
+                                .unwrap();
+    let mut game: Game= Game::new(width, height);
+
+    println!("hello world");
 }
