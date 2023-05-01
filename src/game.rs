20f8f13d744631bfb1ad72f791610f955dad2fe8
MubarakOke <okeolamubarak18@gmail.com>
2023-05-01 21:13:18 +0100

    update game module

--- src/game.rs
+++ src/game.rs
@@ -10,3 +10,21 @@ pub struct Game {
     width: i32,
     height: i32
 }
+
+impl Game {
+    pub fn new(width: i32, height: i32) -> Self {
+        Game { width, height }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d){
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, self.height-1, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
+        draw_rectangle(BORDER_COLOR, 0, self.width-1, 1, self.height, con, g);
+
+    }
+
+    pub fn update(&mut self, delta_time: f64){
+        
+    }
+}
