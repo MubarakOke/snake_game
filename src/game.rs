e92227b299fa98a4108d26c0831eaba7cf1629fc
MubarakOke <okeolamubarak18@gmail.com>
2023-05-03 23:11:25 +0100

    added food

--- src/game.rs
+++ src/game.rs
@@ -5,7 +5,7 @@ use rand::{thread_rng, Rng};
 use crate::draw::{draw_block, draw_rectangle}; 
 
 const BORDER_COLOR: Color= [0.0, 0.0, 0.0, 1.0];
-const FOOD_COLOR: Color= [0.0, 0.0, 0.0, 1.0];
+const FOOD_COLOR: Color= [0.8, 0.0, 0.0, 1.0];
 
 pub struct Game {
     width: i32,
@@ -45,6 +45,7 @@ impl Game {
 
         self.food_x= new_x;
         self.food_y= new_y;
+        self.food_exists= true;
         
     }
 }
