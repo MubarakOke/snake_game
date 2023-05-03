49066a03a5f56de4e2e8579bc11f292c67d1f0ca
MubarakOke <okeolamubarak18@gmail.com>
2023-05-03 23:01:49 +0100

    added food

--- src/game.rs
+++ src/game.rs
@@ -5,18 +5,26 @@ use rand::{thread_rng, Rng};
 use crate::draw::{draw_block, draw_rectangle}; 
 
 const BORDER_COLOR: Color= [0.0, 0.0, 0.0, 1.0];
+const FOOD_COLOR: Color= [0.0, 0.0, 0.0, 1.0];
 
 pub struct Game {
     width: i32,
-    height: i32
+    height: i32,
+
+    food_exists: bool,
+    food_x: i32,
+    food_y: i32,
 }
 
 impl Game {
     pub fn new(width: i32, height: i32) -> Game {
-        Game { width, height }
+        Game { width, height, food_exists: false, food_x: 0, food_y: 0 }
     }
 
     pub fn draw(&self, con: &Context, g: &mut G2d){
+        if self.food_exists {
+            draw_block(FOOD_COLOR, self.food_x, self.food_y, con, g);
+        }
         draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
         draw_rectangle(BORDER_COLOR, 0, self.height-1, self.width, 1, con, g);
         draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
@@ -25,6 +33,13 @@ impl Game {
     }
 
     pub fn update(&mut self, delta_time: f64){
+        if !self.food_exists{
+            self.add_food();
+        }
+    }
 
+    pub fn add_food(&mut self){
+        let rng= thread_rng();
+        let
     }
 }
